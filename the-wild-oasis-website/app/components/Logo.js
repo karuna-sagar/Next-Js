bebce5ee17ebd6077797d5d739dbd9a99cf7ab20
Karuna sagar <karunasagar129130@gmail.com>
2025-02-18 23:48:45 +0530

    `Added logo component and integrated it into the layout`

--- the-wild-oasis-website/app/components/Logo.js
+++ the-wild-oasis-website/app/components/Logo.js
@@ -1,7 +1,15 @@
 function Logo() {
   return (
-    <a href="/" className="flex items-center gap-4 z-10">
-      <img height="60" width="60" alt="The Wild Oasis logo" />
+    <a
+      href="/"
+      className="flex items-center gap-4 z-10"
+    >
+      <img
+        src="/logo.png"
+        height="60"
+        width="60"
+        alt="The Wild Oasis logo"
+      />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
