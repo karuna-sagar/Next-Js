ebee9b49a2904adf7549f53e5024efc7a0343a27
Karuna sagar <karunasagar129130@gmail.com>
2025-02-24 15:17:01 +0530

    `feat: Update CabinCard component to use Image component from next/image, update Supabase client configuration, and modify cabins page to fetch data from a new data service`

--- the-wild-oasis-website/app/_lib/supabase.js
+++ the-wild-oasis-website/app/_lib/supabase.js
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
