9bdb8cbc51d68e791eb8848abd7e3990b41e86bc
Karuna sagar <karunasagar129130@gmail.com>
2025-02-24 23:29:12 +0530

    `Refactor cabin listing: replaced CabinCard with CabinList component` try to implement suspense

--- the-wild-oasis-website/app/cabins/page.js
+++ the-wild-oasis-website/app/cabins/page.js
@@ -1,14 +1,10 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import CabinList from "../_components/CabinList";
 export const metadata = {
   title: "Cabins",
 };
 
 export default async function Page() {
   // CHANGE
-  // console.log("starting");
-  const cabins = await getCabins();
-  // console.log(cabins);
 
   return (
     <div>
@@ -24,16 +20,7 @@ export default async function Page() {
         to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard
-              cabin={cabin}
-              key={cabin.id}
-            />
-          ))}
-        </div>
-      )}
+      <CabinList />
     </div>
   );
 }
