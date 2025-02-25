4b5bb05a334138849c21fbcb4f25887a74b86fbc
Karuna sagar <karunasagar129130@gmail.com>
2025-02-26 03:08:40 +0530

    `Update SideNavigation.js to change bg-primary-900 to bg-primary-800 in hover state`

--- the-wild-oasis-website/app/_components/SideNavigation.js
+++ the-wild-oasis-website/app/_components/SideNavigation.js
@@ -36,7 +36,7 @@ function SideNavigation() {
           <li key={link.name}>
             <Link
               className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
-                pathName === link.href ? "bg-primary-900" : ""
+                pathName === link.href ? "bg-primary-800" : ""
               }`}
               href={link.href}
             >
