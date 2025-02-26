88ea81f3bf20a8ab48f3b00b68b994852ab795f9
Karuna sagar <karunasagar129130@gmail.com>
2025-02-26 19:46:48 +0530

    `Update Reservation component to pass props to DateSelector and ReservationForm`

--- the-wild-oasis-website/app/_components/Reservation.js
+++ the-wild-oasis-website/app/_components/Reservation.js
@@ -10,8 +10,12 @@ export default async function Reservation({ cabin }) {
   ]);
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-      <DateSelector />
-      <ReservationForm />
+      <DateSelector
+        cabin={cabin}
+        settings={settings}
+        bookedDates={bookedDates}
+      />
+      <ReservationForm cabin={cabin} />
     </div>
   );
 }
