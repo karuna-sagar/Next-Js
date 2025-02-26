d4e6c35e308aca2b2d3edde52e08936fe71fa3d6
Karuna sagar <karunasagar129130@gmail.com>
2025-02-26 19:51:10 +0530

    `Added Spinner component and Suspense wrapper to Reservation component in cabin page`

--- the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,7 +1,9 @@
 import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
 import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 // export const metadata = {
 //   title: "Cabin",
@@ -67,7 +69,9 @@ export default async function Page({ params }) {
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
           Reserve {name} today. Pay on arrival.
         </h2>
-        <Reservation cabin={cabin} />
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
       </div>
     </div>
   );
