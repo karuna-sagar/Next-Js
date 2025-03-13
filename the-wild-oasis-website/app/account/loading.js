e5e9e0c361f8e33f030bbc1230a51f5344707dcd
Karuna sagar <karunasagar129130@gmail.com>
2025-03-14 04:47:00 +0530

    spinner is added in account

---
+++ the-wild-oasis-website/app/account/loading.js
@@ -0,0 +1,8 @@
+// import SpinnerMini from "../_components/SpinnerMini";
+// import Spinner from "./_components/Spinner";
+
+import Spinner from "../_components/Spinner";
+
+export default function loading() {
+  return <Spinner />;
+}
