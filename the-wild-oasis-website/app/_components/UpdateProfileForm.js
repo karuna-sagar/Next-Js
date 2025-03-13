50798edd643698aa0b560afc491779dc0b9b247e
Karuna sagar <karunasagar129130@gmail.com>
2025-03-14 00:41:38 +0530

    `Refactor UpdateProfileForm and Page components to use SubmitButton component`

--- the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -2,7 +2,7 @@
 
 import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
-
+import SubmitButton from "./SubmitButton";
 export default function UpdateProfileForm({ guest, children }) {
   const { fullName, email, nationality, nationalID, countryFlag } = guest;
   return (
@@ -51,20 +51,8 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
