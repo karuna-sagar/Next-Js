49de7b17c3825126390a2e877c688c62111412c6
Karuna sagar <karunasagar129130@gmail.com>
2025-03-14 03:33:00 +0530

    `Added ReservationList component and replaced hardcoded reservation list with it`

---
+++ the-wild-oasis-website/app/_components/ReservationList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReservationCard from "./ReservationCard";
+
+function ReservationList({ bookings }) {
+  return (
+    <ul className="space-y-6">
+      {bookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
