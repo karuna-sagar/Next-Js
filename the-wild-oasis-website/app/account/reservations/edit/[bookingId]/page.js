79ba01459bd0d17586e0d0646dd1bc122513db39
Karuna sagar <karunasagar129130@gmail.com>
2025-03-13 19:21:49 +0530

    `Deleted reservation-edit-page.js file`

--- the-wild-oasis-website/app/account/reservations/edit/[bookingId]/page.js
+++ the-wild-oasis-website/app/account/reservations/edit/[bookingId]/page.js
@@ -1,12 +1,13 @@
-export default function Page() {
+export default function Page({ params }) {
   // CHANGE
-  const reservationId = 23;
+  // const reservationId = 23;
+  const bookingId = params;
   const maxCapacity = 23;
 
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Edit Reservation #{reservationId}
+        Edit Reservation #{bookingId}
       </h2>
 
       <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
@@ -18,11 +19,17 @@ export default function Page() {
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
           >
-            <option value="" key="">
+            <option
+              value=""
+              key=""
+            >
               Select number of guests...
             </option>
             {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
-              <option value={x} key={x}>
+              <option
+                value={x}
+                key={x}
+              >
                 {x} {x === 1 ? "guest" : "guests"}
               </option>
             ))}
