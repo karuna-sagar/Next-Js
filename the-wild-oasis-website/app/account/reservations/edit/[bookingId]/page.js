220801ab57de40538a58b6466b8e1e18c27e867e
Karuna sagar <karunasagar129130@gmail.com>
2025-03-13 23:11:04 +0530

    `Update textarea default value in edit reservation page`

--- the-wild-oasis-website/app/account/reservations/edit/[bookingId]/page.js
+++ the-wild-oasis-website/app/account/reservations/edit/[bookingId]/page.js
@@ -47,7 +47,7 @@ export default async function Page({ params }) {
           </label>
           <textarea
             name="observations"
-            defaultChecked={observations}
+            defaultValue={observations}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
