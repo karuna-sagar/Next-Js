733b3b9425ec8f56c6a6cbd2fc5035c9d0fa9325
Karuna sagar <karunasagar129130@gmail.com>
2025-03-14 03:46:17 +0530

    `Update ReservationList component with optimistic updates and delete functionality`

--- the-wild-oasis-website/app/_components/ReservationList.js
+++ the-wild-oasis-website/app/_components/ReservationList.js
@@ -1,13 +1,22 @@
 "use client";
-import React from "react";
+import React, { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
 
-function ReservationList({ bookings }) {
+async function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    () => {}
+  );
+  async function handleDelete(bookingId) {
+    await deleteReservation(bookingId);
+  }
   return (
     <ul className="space-y-6">
       {bookings.map((booking) => (
         <ReservationCard
           booking={booking}
+          onDelete={handleDelete}
           key={booking.id}
         />
       ))}
